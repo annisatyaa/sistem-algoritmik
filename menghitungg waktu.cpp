ee8027f7a6bbe27c7c1a1a3372baa513e075a87e
annisatyaa <141383866+annisatyaa@users.noreply.github.com>
2023-10-09 13:01:56 +0700

    Add files via upload

---
+++ menghitungg waktu.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+// Headers
+string toString (double);
+int toInt (string);
+double toDouble (string);
+
+int main() {
+    int s;
+    int v;
+    double srad;
+    double jarak;
+
+    cin >> s;
+    cin >> v;
+    srad = s * (3.14 / 180);
+    jarak = v * v * sin(2 * srad);
+    cout << jarak << endl;
+    return 0;
+}
+
+// The following implements type conversion functions.
+string toString (double value) { //int also
+    stringstream temp;
+    temp << value;
+    return temp.str();
+}
+
+int toInt (string text) {
+    return atoi(text.c_str());
+}
+
+double toDouble (string text) {
+    return atof(text.c_str());
+}
+
